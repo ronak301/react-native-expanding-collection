cb717606d80a6f656604d2a97ecf8d748107f81e
ronakkothari301 <rkothari@tekion.com>
2020-07-30 13:50:46 +0530

    adding linear gradient colors

--- src/styles/index.style.js
+++ src/styles/index.style.js
@@ -3,8 +3,8 @@ import { StyleSheet } from 'react-native';
 export const colors = {
   black: '#1a1917',
   gray: '#888888',
-  background1: '#B721FF',
-  background2: '#21D4FD',
+  background1: '#FF928B',
+  background2: '#FFAC81',
 };
 
 export default StyleSheet.create({
